d9a2304dfb8f77221e79c67e64b94a80dd785e65
SemCaputo <109221534+SemCaputo@users.noreply.github.com>
2022-10-09 16:58:06 +0100

    typescript Coursework, TS compiling options

---
+++ ConfiguringTypescript/dist/farmstand.js
@@ -0,0 +1,6 @@
+"use strict";
+const printProductSummary = (product) => {
+    console.log(`${product.name} - $${product.price}`);
+};
+//use -w [filename] to watch and compile the ts file as it changes.
+// use tsc to compile any file within the folder after changes are saved.
---
+++ ConfiguringTypescript/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// use tsc --init to create a tsconfig.json file to add specific compiling options
+const norma = {
+    breed: 'Silkie',
+    eggsPerWeek: 3,
+    name: 'Norma'
+};
